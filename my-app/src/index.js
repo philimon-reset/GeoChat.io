3dd51fc00aa48ecd1699c3617626a6ffdb287025
Hizkyas Tekle <hizkyastekle@gmail.com>
2022-02-23 10:27:22 +0300

    proxy working

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './Pages/Landing/App';
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import Home from './Pages/Home/home'
 import {Login} from './Pages/Login/Entry'
 import {Register} from './Pages/Login/Entry'
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -19,8 +17,3 @@ ReactDOM.render(
 </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
